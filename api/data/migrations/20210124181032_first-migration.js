659e736efca58efa59f5e69a81d29bbcd882f4ec
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-07-26 15:43:15 -0500

    first changes

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,38 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("username", 200).notNullable();
+      users.string("password", 200).notNullable();
+      users.timestamps(false, true);
     })
-}
+    .createTable("roles", (roles) => {
+      roles.increments("role_id");
+      roles.string("role_type").notNullable().defaultTo("guest");
+    })
+    .createTable("user_roles", (userRoles) => {
+      userRoles.increments("userRole_id");
+      userRoles
+        .integer("role_id")
+        .unsigned()
+        .notNullable()
+        .references("role_id")
+        .inTable("roles")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      userRoles
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema.dropTableIfExists("user_roles");
+  await knex.schema.dropTableIfExists("roles");
+  await knex.schema.dropTableIfExists("users");
+};
