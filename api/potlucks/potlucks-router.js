54d18aa1e726509a0e7afe2da3fc3233b0550847
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-07-27 12:58:36 -0500

    added error handling middleware and first end point

--- api/potlucks/potlucks-router.js
+++ api/potlucks/potlucks-router.js
@@ -1,11 +1,16 @@
 const router = require('express').Router()
+const Potlucks = require('./potlucks-model')
 
 // need to get, add, update, and delete potlucks
 
 // router.get('', (req, res, next) => {})
 
 router.get('/', (req, res, next) => {
-  
+  Potlucks.getAll()
+    .then(potlucks => {
+      res.json(potlucks)
+    })
+    .catch(next)
 })
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -30,4 +30,11 @@ server.post('/api/users', async (req, res) => {
   res.status(201).json(await insertUser(req.body))
 })
 
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
 module.exports = server
