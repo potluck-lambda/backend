7f67635bf22b80a319147c44a1a0056b1a9c9f3a
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-07-27 17:13:44 -0500

    can post potlucks correctly

--- api/potlucks/potlucks-model.js
+++ api/potlucks/potlucks-model.js
@@ -4,8 +4,17 @@ function getAll() {
   return db('potlucks')
 }
 
-function addPotluck(potluck) {
-  return db('potlucks').insert(potluck, [])
+async function addPotluck(potluck) {
+  const [newPotluck] = await db('potlucks').insert(potluck, 
+    ["organizer_id",
+    "date",
+    "time",
+    "street_number",
+    "street_name",
+    "state",
+    "zip_code"
+    ])
+  return newPotluck
 }
 
 module.exports = {
